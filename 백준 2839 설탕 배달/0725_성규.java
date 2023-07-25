0d9bdc81c45c687cf707fad04a16eca12fe7e1cb
tt <94626920+Gyu-S@users.noreply.github.com>
2023-07-25 16:31:21 +0900

    Update 0725_성규.java

--- 백준 2839 설탕 배달/0725_성규.java
+++ 백준 2839 설탕 배달/0725_성규.java
@@ -6,30 +6,26 @@ class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int cnt = 0;
-		int j =0;
-		int min = Integer.MAX_VALUE;
-		while (true)
+		int min = -1;
+		int cnt = n / 5;
+		
+		while (cnt >= 0)
 		{
-			int temp = n;
-			cnt = 0;
-			for(int i=0;i<j;i++)
+			int temp = n - (5 * cnt);
+			
+			if(temp == 0)
 			{
-				temp -= 5;
-				cnt++;
-			}
-			if (temp <= 0)
-			{
-				if (n % 5 == 0)
-					min = Math.min(min, n / 5);
+				min = cnt;
 				break;
 			}
+			
 			if(temp % 3 == 0)
-				min = Math.min(min, (cnt + (temp / 3)));
-			j++;
+			{
+				min = cnt + (temp / 3);
+				break;
+			}
+			cnt--;
 		}
-		if (min == Integer.MAX_VALUE)
-			min = -1;
 		System.out.println(min);
 	}
 }
