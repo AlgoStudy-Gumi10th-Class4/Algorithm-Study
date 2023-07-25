a63ff0a3dcd333ddee4b32fb0a1cfbc2b43f7e21
Shin Juyong <cheesecat47@gmail.com>
2023-07-25 07:54:19 +0000

    BOJ 2839 채점 결과 업데이트

--- 백준 2839 설탕 배달/Nojam2839_230724_jy.java
+++ 백준 2839 설탕 배달/Nojam2839_230724_jy.java
@@ -1,11 +1,15 @@
 // 설탕 배달 (실버4) https://noj.am/2839
+// 
 // 현재 무게 w를 만들 수 이는 설탕 봉지 최소 개수는
 // min(w-3 무게 봉지 수 + 1, w-5 봉지 수 + 1)
+//
+// https://www.acmicpc.net/status?from_mine=1&problem_id=2839&user_id=cheesecat47
+// 17708KB / 216ms
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Nojam2839_230724 {	
+public class Nojam2839_230724_jy {	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
