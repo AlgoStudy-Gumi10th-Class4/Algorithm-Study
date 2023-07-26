89dfc41bf086c89274952014a556fc35725ea330
tt <94626920+Gyu-S@users.noreply.github.com>
2023-07-26 15:19:44 +0900

    Update 0726성규.java

--- 백준 2343. 기타 레슨/0726성규.java
+++ 백준 2343. 기타 레슨/0726성규.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+// 블루레이 개수가 제한되어 있으므로, 입력 받아 온 개수를 사용해 평균을 구한다.
+// 평균을 기준으로 sum배열에 arr 값을 순서대로 입력해가며 만약 제한된 블루레이 개수를 넘어가면 평균 값을 더해가며 반복한다.
+// 최소 값(평균 값)이 계속 더해지면서 반복하기 때문에 만약 arr 값이 다 들어간다면 그 평균 값이 최소 값이기 때문에 그 즉시
+// while문을 빠져나와 끝낸다.
 class Solution
 {	
 	static int avg;
