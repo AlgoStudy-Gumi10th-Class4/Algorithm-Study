4c83734096b8f69556a2e697e0c3fbcc1c18abb2
tt <94626920+Gyu-S@users.noreply.github.com>
2023-07-26 15:55:03 +0900

    Update 0726성규.java

--- 백준 2343. 기타 레슨/0726성규.java
+++ 백준 2343. 기타 레슨/0726성규.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Arrays;
 import java.util.Scanner;
 // 블루레이 개수가 제한되어 있으므로, 입력 받아 온 개수를 사용해 평균을 구한다.
@@ -59,4 +60,55 @@ class Solution
 		}
 	}
 	
+}
+*/
+// 위 코드는 시간이 너무 오래 걸려 다시 만들었다.
+// cnt 값이 m 보다 크면 mid 값이 작기 때문에 start 값을 늘려간다.
+// cnt 값이 m 보다 작으면 mid 값이 크기 때문에 end 값을 줄여간다.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution
+{		
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] arr = new int[n];
+		
+		int end = 0;
+		int start = Integer.MIN_VALUE;
+		
+		for(int i=0;i<n;i++)
+		{
+			arr[i] = sc.nextInt();
+			end += arr[i];
+			start = Math.max(start, arr[i]);
+		}
+		sc.close();
+		
+		int mid, cnt, sum;
+		while(start <= end)
+		{
+			mid = (start + end) / 2;
+			cnt = 0;
+			sum = 0;
+			for(int i=0;i<n;i++)
+			{
+				if (sum + arr[i] > mid) {
+					cnt++;
+					sum = 0;
+				}
+				sum += arr[i];
+			}
+			
+			if (sum != 0)	cnt++;
+			if (cnt > m)	start = mid + 1;
+			else			end = mid - 1;
+		}
+		
+		System.out.println(start);
+	}	
 }
