70666c6c595e6df41c7780bf49ab4b91003a64a6
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2023-07-28 10:08:13 +0900

    Create yeah230728.java

---
+++ 백준 2961. 도영이가 만든 맛있는 음식/yeah230728.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+//모든 조합을 만들어보는 문제
+//리스트에 하나의 요리만 들어간 메뉴를 넣고 이전에 입력한 메뉴에 현재 요리를 추가.
+//교체가 아니라 추가이기 때문에 모든 조합을 확인할 수 있다.
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int res = Integer.MAX_VALUE;
+		ArrayList<Food> arr = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			String[] tmp = br.readLine().split(" ");
+			int s = Integer.parseInt(tmp[0]);
+			int b = Integer.parseInt(tmp[1]);
+
+			arr.add(new Food(s, b));
+			if (res > Math.abs(s - b)) {
+				res = Math.abs(s - b);
+			}
+
+			if (i != 0) {
+				int len = arr.size()-1;
+				for (int j = 0; j < len; j++) {
+					Food cur = arr.get(j);
+					int ts = cur.sour * s;
+					int tb = cur.bitter + b;
+					arr.add(new Food(ts, tb));
+					if (res > Math.abs(ts - tb)) {
+						res = Math.abs(ts - tb);
+					}
+				}
+			}
+		}
+		System.out.println(res);
+	}
+
+}
+
+class Food {
+	int sour;
+	int bitter;
+
+	public Food(int sour, int bitter) {
+		super();
+		this.sour = sour;
+		this.bitter = bitter;
+	}
+}
